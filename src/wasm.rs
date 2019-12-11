89d900d0b9add96778049bbf54bcb1642af6cb61
rhysd <lin90162@yahoo.co.jp>
2019-12-11 15:02:50 +0900

    add monolith_of_html to make monolith for given HTML text

--- src/wasm.rs
+++ src/wasm.rs
@@ -5,10 +5,10 @@ use crate::html::{html_to_dom, stringify_document, walk_and_embed_assets};
 use crate::http::retrieve_asset;
 use crate::utils::is_valid_url;
 
+// Entrypoints for WebAssembly port of monolith. This function will be called from JavaScript
 
-// Entrypoint for WebAssembly port of monolith. This function will be called from JavaScript
-#[wasm_bindgen]
-pub async fn monolith(
+#[wasm_bindgen(js_name = monolithOfUrl)]
+pub async fn monolith_of_url(
     url_target: String,
     no_css: bool,
     no_frames: bool,
@@ -35,6 +35,47 @@ pub async fn monolith(
     )
     .await?;
     let dom = html_to_dom(&data);
+
+    walk_and_embed_assets(
+        cache,
+        &final_url,
+        &dom.document,
+        no_css,
+        no_js,
+        no_images,
+        user_agent.as_str(),
+        silent,
+        insecure,
+        no_frames,
+    ).await?;
+
+    let html = stringify_document(
+        &dom.document,
+        no_css,
+        no_frames,
+        no_js,
+        no_images,
+        isolate,
+    );
+
+    Ok(html)
+}
+
+#[wasm_bindgen(js_name = monolithOfHtml)]
+pub async fn monolith_of_html(
+    html: String,
+    final_url: String,
+    no_css: bool,
+    no_frames: bool,
+    no_images: bool,
+    no_js: bool,
+    insecure: bool,
+    isolate: bool,
+    silent: bool,
+    user_agent: String,
+) -> Result<String, JsValue> {
+    let cache = &mut HashMap::new();
+    let dom = html_to_dom(&html);
 
     walk_and_embed_assets(
         cache,
